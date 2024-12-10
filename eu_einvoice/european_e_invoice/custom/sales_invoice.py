1c33825ace5f45a5e914176de310491568dc5244
barredterra <14891507+barredterra@users.noreply.github.com>
2024-12-10 17:37:31 +0100

    fix(BR-CO-16): oustanding == grand_total - paid
    Amount due for payment (BT-115) = Invoice total amount with VAT (BT-112) -Paid amount (BT-113) +Rounding amount (BT-114).
    
    Resolves #14

--- eu_einvoice/european_e_invoice/custom/sales_invoice.py
+++ eu_einvoice/european_e_invoice/custom/sales_invoice.py
@@ -502,8 +502,7 @@ class EInvoiceGenerator:
 		)
 		self.doc.trade.settlement.monetary_summation.grand_total = self.invoice.grand_total
 
-		if self.invoice.is_return and not self.invoice.update_outstanding_for_self:
-			# We paid the grand total by reducing the original invoice
+		if self.invoice.outstanding_amount == 0:
 			self.doc.trade.settlement.monetary_summation.prepaid_total = self.invoice.grand_total
 		else:
 			self.doc.trade.settlement.monetary_summation.prepaid_total = self.invoice.total_advance
